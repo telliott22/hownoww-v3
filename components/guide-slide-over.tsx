a7ba4d760ddb42c80118a360632067cdfeb13106
Tim Elliott <telliott22@gmail.com>
2022-02-23 19:31:38 +0100

    feat: tweaked gluide over style

--- components/guide-slide-over.tsx
+++ components/guide-slide-over.tsx
@@ -10,11 +10,11 @@ const GuideSlideOver: FunctionComponent<{ pageSlugs: GuidePageNavItem[], open: b
 
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={() => setOpen(false)}>
+      <Dialog as="div" className="absolute inset-0 overflow-hidden z-10" onClose={() => setOpen(false)}>
         <div className="absolute inset-0 overflow-hidden">
           <Dialog.Overlay className="absolute inset-0" />
 
-          <div className="fixed inset-y-0 left-0 lg:pr-10 max-w-full flex">
+          <div className="absolute inset-y-0 left-0 lg:pr-10 max-w-full flex">
             <Transition.Child
               as={Fragment}
               enter="transform transition ease-in-out duration-500 sm:duration-700"
